75b1f1b5905f7c8f113c2f47a2fe7b474651641b
Ian Butterworth <i.r.butterworth@gmail.com>
2019-03-23 19:50:27 -0400

    new tests for travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using Darknet
 using Test
 
-darknet_img = Darknet.array_to_image(rand(Float32,100,100))
+darknet_img = Darknet.array_to_image(rand(Float32,100,10))
 
 @test typeof(darknet_img) == Darknet.image
+@test darknet_img.w == 100
+@test darknet_img.h == 10
+@test darknet_img.c == 1
