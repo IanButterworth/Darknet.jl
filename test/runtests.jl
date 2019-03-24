aa32c8088fcee042035068161fee142e6e482b02
Ian Butterworth <i.r.butterworth@gmail.com>
2019-03-24 00:27:15 -0400

    Correct w & h

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,8 @@ using Test
 
 @testset "Create Darknet image" begin
     darknet_img = Darknet.array_to_image(rand(Float32,100,10))
-
     @test typeof(darknet_img) == Darknet.image
-    @test darknet_img.w == 100
-    @test darknet_img.h == 10
+    @test darknet_img.h == 100
+    @test darknet_img.w == 10
     @test darknet_img.c == 1
 end
