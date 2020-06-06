c211ec1a25f38984bd6a240065fbff99851acd82
Ian <i.r.butterworth@gmail.com>
2020-06-06 18:34:04 -0400

    move to Darknet_jll backend

--- src/Darknet.jl
+++ src/Darknet.jl
@@ -1,22 +1,10 @@
 module Darknet
 using Libdl, CEnum
 
-# Load in `deps.jl`, complaining if it does not exist
-const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
-if !isfile(depsjl_path)
-    error("Darknet not installed properly, run `] build Darknet', restart Julia and try again")
-end
-include(depsjl_path)
+using Darknet_jll
 
-# Module initialization function
-function __init__()
-    check_deps()
-end
-
-
-include("ctypes.jl")
+include(joinpath(@__DIR__, "..", "gen", "ctypes.jl"))
 export Ctm, Ctime_t, Cclock_t
-
 include(joinpath(@__DIR__, "..", "gen", "libdarknet_common.jl"))
 include(joinpath(@__DIR__, "..", "gen", "libdarknet_api.jl"))
 
