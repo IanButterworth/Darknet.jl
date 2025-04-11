5ed0840769f70a09b13bc0196fd5e67f5b472582
Ian Butterworth <i.r.butterworth@gmail.com>
2025-04-11 14:31:50 -0400

    Register finalizers to clean up nets. Test tweaks. (#26)

--- src/Darknet.jl
+++ src/Darknet.jl
@@ -47,4 +47,30 @@ function download_defaults()
     !isfile(weightsfile) && Downloads.download("https://github.com/IanButterworth/Darknet.jl/releases/download/v0.3.2/yolov3-tiny.weights", weightsfile)
 end
 
+# Define a mutable struct to wrap the pointer.
+mutable struct Network
+    ptr::Ptr{Darknet.network}
+end
+Base.unsafe_convert(::Type{Ptr{Darknet.network}}, net::Darknet.Network) = net.ptr
+
+# register finalizers on load
+function load_network(cfg, weights, clear)
+    # Must rename this method after generating the wrapper in LibDarknet.jl to avoid name conflict
+    net_ptr = _load_network(cfg, weights, clear)
+    net = Network(net_ptr)
+    finalizer(net) do n
+        free_network_ptr(n.ptr)
+    end
+    return net
+end
+function load_network_custom(cfg, weights, clear, batch)
+    # Must rename this method after generating the wrapper in LibDarknet.jl to avoid name conflict
+    net_ptr = _load_network_custom(cfg, weights, clear, batch)
+    net = Network(net_ptr)
+    finalizer(net) do n
+        free_network_ptr(n.ptr)
+    end
+    return net
+end
+
 end # module Darknet
--- src/testing.jl
+++ src/testing.jl
@@ -2,20 +2,30 @@
 function detect(net, meta, img; thresh=0.5, hier_thresh=0.5, nms=0)
     num_dets = Ref(Cint(0))
     network_predict_image(net, img)
-    dets = get_network_boxes(net, img.w, img.h, thresh, hier_thresh, C_NULL, 0, num_dets, 0)
+    dets = get_network_boxes(
+        net,         # The YOLO neural network model (contains weights & config)
+        img.w,       # Width of the input image (after preprocessing if any)
+        img.h,       # Height of the input image (after preprocessing if any)
+        thresh,      # Detection threshold (filter out boxes with lower confidence)
+        hier_thresh, # Hierarchical threshold (used in hierarchical classification, typically ignored in standard YOLO)
+        C_NULL,      # Class mapping array (NULL means no remapping of class IDs)
+        0,           # Whether to return relative coordinates (0 = absolute pixel values)
+        num_dets,    # Pointer to an integer storing the number of detections
+        0            # Letterboxing flag (0 = image was resized without padding)
+    )
     if nms > 0
         do_nms_sort(dets, num_dets[], meta.classes, nms)
     end
     res = []
     detections = unsafe_wrap(Array, dets, num_dets[])
     names = unsafe_wrap(Array, meta.names, meta.classes)
-    for j in 1:num_dets[]
-        prob = unsafe_wrap(Array, detections[j].prob, meta.classes)
-        for i in 1:meta.classes
-            if prob[i] > 0
-                b = detections[j].bbox
-                nameTag = unsafe_string(names[i])
-                push!(res,(nameTag, prob[i], (b.x, b.y, b.w, b.h)))
+    for detection in detections
+        probs = unsafe_wrap(Array, detection.prob, meta.classes)
+        b = detection.bbox
+        for (i, prob) in enumerate(probs)
+            if prob > 0
+                name = unsafe_string(names[i])
+                push!(res, (name, prob, (b.x, b.y, b.w, b.h)))
             end
         end
     end
